f09c2f518d1fe9757c4456ce6365566b119488a7
Dinty1 <dintybmrt@gmail.com>
2023-06-11 23:16:16 +0100

    Rearrange

--- common/src/main/java/com/discordsrv/common/config/documentation/DocumentationURLs.java
+++ common/src/main/java/com/discordsrv/common/config/documentation/DocumentationURLs.java
@@ -1,5 +1,5 @@
 package com.discordsrv.common.config.documentation;
 
-public class DocumentationConfig {
+public class DocumentationURLs {
     public static final String CREATE_TOKEN = "https://docs.discordsrv.com/installation/initial-setup/#setting-up-the-bot";
 }
