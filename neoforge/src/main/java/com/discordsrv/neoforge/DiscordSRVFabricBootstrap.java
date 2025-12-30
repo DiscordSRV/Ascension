3f45611c9c87630a1c173f0edec69566303aa1c9
IxPrumxI <walidaqeel@gmail.com>
2025-12-30 17:28:30 +0300

    Work in progress

---
+++ neoforge/src/main/java/com/discordsrv/neoforge/DiscordSRVFabricBootstrap.java
@@ -0,0 +1,133 @@
+/*
+ * This file is part of DiscordSRV, licensed under the GPLv3 License
+ * Copyright (c) 2016-2025 Austin "Scarsz" Shapiro, Henri "Vankka" Schubin and DiscordSRV contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.discordsrv.neoforge;
+
+import com.discordsrv.common.abstraction.bootstrap.IBootstrap;
+import com.discordsrv.common.abstraction.bootstrap.LifecycleManager;
+import com.discordsrv.common.core.logging.Logger;
+import com.discordsrv.common.core.logging.backend.impl.Log4JLoggerImpl;
+import dev.vankka.dependencydownload.classpath.ClasspathAppender;
+import dev.vankka.mcdependencydownload.fabric.classpath.FabricClasspathAppender;
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.impl.FabricLoaderImpl;
+import net.minecraft.server.MinecraftServer;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Optional;
+
+public class DiscordSRVFabricBootstrap implements DedicatedServerModInitializer, IBootstrap {
+    private final static String DEPENDENCIES_RUNTIME = /*$ dependencies_file*/"dependencies/runtimeDownload-1.21.9.txt";
+
+    private final Logger logger;
+    private final ClasspathAppender classpathAppender;
+    private final LifecycleManager lifecycleManager;
+    private final Path dataDirectory;
+    private MinecraftServer minecraftServer;
+    private FabricDiscordSRV discordSRV;
+
+    public DiscordSRVFabricBootstrap() {
+        this.logger = new Log4JLoggerImpl(LogManager.getLogger("DiscordSRV"));
+        this.classpathAppender = new FabricClasspathAppender();
+        this.dataDirectory = FabricLoader.getInstance().getConfigDir().resolve("DiscordSRV");
+        try {
+            this.lifecycleManager = new LifecycleManager(
+                    this.logger,
+                    dataDirectory,
+                    Collections.singletonList(DEPENDENCIES_RUNTIME),
+                    classpathAppender
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.minecraftServer = null;
+    }
+
+    @Override
+    public void onInitializeServer() {
+        ServerLifecycleEvents.SERVER_STARTING.register(minecraftServer -> {
+            this.minecraftServer = minecraftServer;
+            lifecycleManager.loadAndEnable(() -> this.discordSRV = new FabricDiscordSRV(this));
+        });
+
+        ServerLifecycleEvents.SERVER_STARTED.register(minecraftServer -> {
+            if (this.discordSRV == null) {
+                this.logger.error("Server started but FabricDiscordSRV hasn't initialized properly.\n" +
+                        "This is likely due to an error during the loading process. Please check the full logs for more details.");
+                return;
+            }
+            this.discordSRV.runServerStarted();
+        });
+
+        ServerLifecycleEvents.SERVER_STOPPING.register(minecraftServer -> {
+            if (this.discordSRV != null) this.discordSRV.runDisable();
+        });
+    }
+
+    @Override
+    public Logger logger() {
+        return logger;
+    }
+
+    @Override
+    public ClasspathAppender classpathAppender() {
+        return classpathAppender;
+    }
+
+    @Override
+    public ClassLoader classLoader() {
+        return getClass().getClassLoader();
+    }
+
+    @Override
+    public LifecycleManager lifecycleManager() {
+        return lifecycleManager;
+    }
+
+    @Override
+    public Path dataDirectory() {
+        return dataDirectory;
+    }
+
+    @Override
+    public String platformVersion() {
+        //? if minecraft: >=1.21.6 {
+        String minecraftVersion = net.minecraft.SharedConstants.getCurrentVersion().name();
+         //?} else {
+        /*String minecraftVersion = net.minecraft.DetectedVersion.BUILT_IN.getName();
+         *///?}
+
+        String loader_version = FabricLoaderImpl.VERSION;
+        Optional<ModContainer> fabricApi = FabricLoader.getInstance().getModContainer("fabric-api");
+        return "Minecraft " + minecraftVersion + " with Fabric Loader " + loader_version + (fabricApi.map(modContainer -> " (Fabric API: " + modContainer.getMetadata().getVersion().getFriendlyString() + ")").orElse(""));
+    }
+
+    public MinecraftServer getServer() {
+        return minecraftServer;
+    }
+
+    public FabricDiscordSRV getDiscordSRV() {
+        return discordSRV;
+    }
+}
