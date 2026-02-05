ab0d3de1851ceac0a8894afbc5c443e9a13c151e
IxPrumxI <walidaqeel@gmail.com>
2026-02-05 10:44:14 +0300

    Fix missing imports

--- modded/src/main/java/com/discordsrv/modded/mixin/ServerPlayerMixin.java
+++ modded/src/main/java/com/discordsrv/modded/mixin/ServerPlayerMixin.java
@@ -27,6 +27,8 @@ import net.minecraft.world.damagesource.CombatTracker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin implements ServerPlayerEntityAccessor {
